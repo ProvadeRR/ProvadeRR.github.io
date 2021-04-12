0a6551feab7c9529c19469f38f1acd70e825a051
KarnozaIT <stanislavkarnoza2021@gmail.com>
2021-04-12 19:31:09 +0300

    slow mode on scroll

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
     $('.nav__item > a').click(function(e){
         e.preventDefault();
        if(this.innerHTML === 'Фестівалі')
-           $("html, body").animate({ scrollTop: $('.festivals').offset().top - 200  });
+           $("html, body").animate({ scrollTop: $('.festivals').offset().top - 200  } , 'slow');
         if(this.innerHTML === 'Головна')
             $("html, body").animate({ scrollTop: 0  });
         if(this.innerHTML === 'Галерея')
-            $("html, body").animate({ scrollTop: $('.galery').offset().top - 200  });
+            $("html, body").animate({ scrollTop: $('.galery').offset().top - 200  }, 'slow');
         if(this.innerHTML === 'Табір')
-            $("html, body").animate({ scrollTop: $('.tabir').offset().top - 200  });
+            $("html, body").animate({ scrollTop: $('.tabir').offset().top - 200  }, 'slow');
         if(this.innerHTML === 'Про нас')
-            $("html, body").animate({ scrollTop: $('.about_us_title').offset().top - 200  });
+            $("html, body").animate({ scrollTop: $('.about_us_title').offset().top - 200  }, 'slow');
         if(this.innerHTML === 'Реквізити та контакти')
-            $("html, body").animate({ scrollTop: $('.footer').offset().top - 200  });
+            $("html, body").animate({ scrollTop: $('.footer').offset().top - 200  }, 'slow');
     });
 });
