6b3aca00e83ab12130c73606a2e93242abcc2b1e
KarnozaIT <stanislavkarnoza2021@gmail.com>
2021-04-15 12:53:58 +0300

    Сделан бургер меню

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 $(document).ready(function(){
     $('.nav__item > a').click(function(e){
         e.preventDefault();
+        if($('.nav').hasClass('open')){
+            document.querySelector('.nav').classList.remove('open');
+        }
+        if($('.nav').hasClass('open-mobile')){
+            document.querySelector('.nav').classList.remove('open-mobile');
+        }
        if(this.innerHTML === 'Фестівалі')
-           $("html, body").animate({ scrollTop: $('.festivals').offset().top - 200  } , 'slow');
+           $("html, body").animate({ scrollTop: $('.festivals').offset().top - 200  } , );
         if(this.innerHTML === 'Головна')
             $("html, body").animate({ scrollTop: 0  });
         if(this.innerHTML === 'Галерея')
-            $("html, body").animate({ scrollTop: $('.galery').offset().top - 200  }, 'slow');
+            $("html, body").animate({ scrollTop: $('.galery').offset().top - 200  }, );
         if(this.innerHTML === 'Табір')
-            $("html, body").animate({ scrollTop: $('.tabir').offset().top - 200  }, 'slow');
+            $("html, body").animate({ scrollTop: $('.tabir').offset().top - 200  },);
         if(this.innerHTML === 'Про нас')
-            $("html, body").animate({ scrollTop: $('.about_us_title').offset().top - 200  }, 'slow');
+            $("html, body").animate({ scrollTop: $('.about_us_title').offset().top - 200  });
         if(this.innerHTML === 'Реквізити та контакти')
-            $("html, body").animate({ scrollTop: $('.footer').offset().top - 200  }, 'slow');
+            $("html, body").animate({ scrollTop: $('.footer').offset().top - 200  });
     });
+
+    $('.burger-menu').click(function() {
+        if (window.innerWidth < 631) {
+            document.querySelector('.nav').classList.remove('open');
+            document.querySelector('.nav').classList.toggle('open-mobile');
+            $('.nav').appendTo($('.header'));
+        } else {
+            document.querySelector('.nav').classList.toggle('open');
+        }
+    })
+
 });
